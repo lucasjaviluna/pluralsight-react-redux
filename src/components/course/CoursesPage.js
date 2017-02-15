520adf8a9931712ab3b18f20c98a448948838fc5
Lucas <lucasjaviluna@gmail.com>
2017-02-15 11:35:49 -0300

    Add thunk to store

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -7,29 +7,29 @@ class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-      course : {
-        title: ""
-      }
-    };
+    // this.state = {
+    //   course : {
+    //     title: ""
+    //   }
+    // };
 
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
+    // this.onTitleChange = this.onTitleChange.bind(this);
+    // this.onClickSave = this.onClickSave.bind(this);
   }
 
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    //alert(`Saving ${this.state.course.title}`);
-
-    //this.props.dispatch(courseActions.createCourse(this.state.course));
-    //al definir la funcion mapDispatchToProps, ya no necesitamos el dispatch aqui!!
-    this.props.actions.createCourse(this.state.course);
-  }
+  // onTitleChange(event) {
+  //   const course = this.state.course;
+  //   course.title = event.target.value;
+  //   this.setState({course: course});
+  // }
+  //
+  // onClickSave() {
+  //   //alert(`Saving ${this.state.course.title}`);
+  //
+  //   //this.props.dispatch(courseActions.createCourse(this.state.course));
+  //   //al definir la funcion mapDispatchToProps, ya no necesitamos el dispatch aqui!!
+  //   this.props.actions.createCourse(this.state.course);
+  // }
 
   courseRow(course, index) {
     return (
@@ -42,9 +42,10 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
+
+        {/* <h2>Add Course</h2>
         <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
-        <input type="submit" onClick={this.onClickSave} value="Save" />
+        <input type="submit" onClick={this.onClickSave} value="Save" /> */}
       </div>
     );
   }
