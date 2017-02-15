c440aac56f393a2be94fecf29c0ee22ea5f9405e
Lucas <lucasjaviluna@gmail.com>
2017-02-15 14:32:55 -0300

    Redirect via React Router Context

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -12,21 +12,48 @@ class ManageCoursePage extends React.Component {
       course: Object.assign({}, this.props.course),
       errors: {}
     };
+
+    this.updateCourseState = this.updateCourseState.bind(this);
+    this.saveCourse = this.saveCourse.bind(this);
+  }
+
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({
+      course: course
+    });
+  }
+
+  saveCourse(event) {
+    event.preventDefault();
+    this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
 
   render() {
     return (
       <CourseForm
-        allAuthors={[]}
+        allAuthors={this.props.authors}
         course={this.state.course}
-        errors={this.state.errors}/>
+        errors={this.state.errors}
+        onChange={this.updateCourseState}
+        onSave={this.saveCourse}/>
     );
   }
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  errors: PropTypes.array
+  errors: PropTypes.array,
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+//Pull in the React Router context so router is available on this.context.router
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object
 };
 
 function mapStateToProps(state, ownProps) {
@@ -39,14 +66,22 @@ function mapStateToProps(state, ownProps) {
     category: ''
   };
 
+  const authorsFormattedForDropdown = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: author.firstName + ' ' + author.lastName
+    };
+  });
+
   return {
-    course: course
+    course: course,
+    authors: authorsFormattedForDropdown
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    //actions: bindActionCreators(actions, dispatch)
+    actions: bindActionCreators(courseActions, dispatch)
   };
 }
 
